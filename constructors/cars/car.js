e087f39ff0ad94765e27e8e19d8a1e3cffda69fa
Samvel Asoyan <samvel24.1998@gmail.com>
2019-10-11 23:59:20 +0400

    homework

--- constructors/cars/car.js
+++ constructors/cars/car.js
@@ -5,7 +5,7 @@ class RollsRoyce{
         this.price = price
     }
     present(){
-        let text = document.querySelector(`.${this.model}>.text`);
+        let text = document.querySelector(`.rr${this.model}>.text`);
         let li1 = document.createElement("li");
         let li2 = document.createElement("li");
         let li3 = document.createElement("li");
@@ -18,7 +18,7 @@ class RollsRoyce{
         list1.appendChild(li2);
         let list2 = document.createElement("ul");
         list2.className = "price";
-        li3.innerHTML = "price";
+        li3.innerHTML = "starting at";
         li4.innerHTML = "$" + this.price;
         list2.appendChild(li3);
         list2.appendChild(li4);
@@ -27,15 +27,34 @@ class RollsRoyce{
     }
 }
 
-let phantom = new RollsRoyce("phantom", 452000);
-let ghost = new RollsRoyce("ghost", 325000);
-let wraith = new RollsRoyce("wraith", 322000);
-let dawn = new RollsRoyce("dawn", 349000);
-let cullinan = new RollsRoyce("cullinan", 327000);
-let blackbadge = new RollsRoyce("black-badge", 325000);
+let phantom = new RollsRoyce("Phantom", 452000);
+let ghost = new RollsRoyce("Ghost", 325000);
+let wraith = new RollsRoyce("Wraith", 322000);
+let dawn = new RollsRoyce("Dawn", 349000);
+let cullinan = new RollsRoyce("Cullinan", 327000);
+let blackbadge = new RollsRoyce("Black-badge", 325000);
 
-let arr = [phantom, ghost, wraith, dawn, cullinan, blackbadge]
+let arr = [phantom, ghost, wraith, dawn, cullinan, blackbadge];
 
 for(let i of arr){
     i.present()
+}
+const change = (x) =>{
+    let main = document.querySelector(".main");
+    main.classList.remove("background");
+    main.classList.add(`change${x}`);
+    let cars = document.querySelectorAll(".car");
+    for(let i of cars){
+        i.style.opacity = 0;
+    }
+    cars[x].style.opacity = 1;
+}
+const rechange = (x) =>{
+    let main = document.querySelector(".main");
+    main.classList.remove(`change${x}`);
+    main.classList.add("background");
+    let cars = document.querySelectorAll(".car");
+    for(let i of cars){
+        i.style.opacity = 1
+    }
 }
